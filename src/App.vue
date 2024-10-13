133f6d1b2868b61e4ad8a82a63a810c8f0c2a6ba
ahmed-test <116157035+themeddev@users.noreply.github.com>
2024-10-13 23:53:16 +0100

    install packages "VueJs, Vite and TailwindCSS"

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import GlobalLayout from './components/GlobalLayout.vue'
+
+</script>
+
+<template>
+  <GlobalLayout>
+    <div class="h-screen w-screen bg-gradient-to-l from-gray-200 via-fuchsia-300 to-stone-100 flex items-center justify-center">
+      <h1>PictoStick</h1>
+    </div>
+  </GlobalLayout>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/GlobalLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-full h-full bg-gradient-to-r from-blue-100 to-blue-500">
+        <slot />
+    </div>
+</template>
